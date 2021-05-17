0aa299591fcaee9a3458803ad3022047bd5ecfb5
VIJAYAKUMAR <pangavijaykumar9966@gmail.com>
2021-05-17 09:27:24 +0530

    project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/styles.css'
+const Navbar =()=>{
+return(
+    <div className ="navbar">
+        <input type ="text" ></input>
+        <button onClick=""> Search</button>
+    </div>
+) 
+
+}
+export default Navbar
